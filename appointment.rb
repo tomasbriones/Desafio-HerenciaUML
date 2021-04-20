20f9ca2a0c38ef637c40e67e7bae601f10dbad3f
Tomas Briones <tbriones@economial.cl>
2021-04-19 22:04:14 -0400

    puts

--- appointment.rb
+++ appointment.rb
@@ -69,3 +69,7 @@ class OneTimeAppointment < Appointment
     
 
 end
+
+puts OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14, 30, 4, 6, 2019)
+#puts DailyAppointment.new('Desafío Latam', 'Educación', 8, 15)
+#puts MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
