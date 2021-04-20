161c9f43a653c48c5d4818aa19b70d6a96443888
Tomas Briones <tbriones@economial.cl>
2021-04-19 22:02:16 -0400

    uptade to_s methods

--- appointment.rb
+++ appointment.rb
@@ -1,4 +1,7 @@
 class Appointment
+
+    attr_reader :location, :purpose, :hour , :min
+
     def initialize(location,purpose,hour,min)
         @location = location
         @purpose = purpose
@@ -7,13 +10,62 @@ class Appointment
     end
 end
 
-class MonthlyAppointment > Appointment
+class MonthlyAppointment < Appointment
+
+    attr_reader :day
+
     def initialize(location,purpose,hour,min,day)
         super(location,purpose,hour,min)
         @day = day
     end
+
+    def occurs_on?(otherday)
+        if self.day == otherday
+            true
+        else
+            false
+        end
+    end
+    
+    def to_s
+        puts "Reunión mensual en #{self.locate} sobre #{self.purpose} a las #{self.hour}:#{self.min}"
+    end
+    
 end
 
-class DailyAppointment > Appointment
+class DailyAppointment < Appointment
+    def occurs_on?(otherhour,min)
+        if self.hour == otherhour && self.min == othermin
+    end
     
+    def to_s
+        puts "Reunión diaria en #{self.location} sobre #{self.purpose} a la(s) #{self.hour}:#{self.min}."     
+    end
+    
+end
+
+class OneTimeAppointment < Appointment
+
+    attr_accessor :day, :month, :year
+
+    def initialize(day,month,year,location,purpose,hour,min)
+        super(location,purpose,hour,min)
+        @day = day
+        @month = month
+        @year = year
+    end
+
+    def occurs_on?(otherDay,otherMonth,otherYear)
+        if self.day = otherDay && self.month ==otherMonth && self.year == otherYear
+            true
+        else
+            false
+        end
+    end
+
+    def to_s
+        puts "Reunión mensual en #{self.location} sobre #{self.purpose} el #{self.day}/#{self.month}/#{year}."
+    end
+    
+
 end
